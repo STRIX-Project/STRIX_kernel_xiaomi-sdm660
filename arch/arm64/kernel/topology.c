801c2746859135918ee1e713e33bb21813f3fddb
Joonwoo Park <joonwoop@codeaurora.org>
2017-06-09 14:06:54 -0700

    sched: energy: calculate and update CPU capacity dynamically
    One SoC can have multiple CPU speedbins which cannot be represented
    with current energy model due to fixed capacity per CPU frequency
    steps.
    
    Provide CPU's all possible frequency steps instead of capacities along
    with corresponding energy costs to be able to support different
    speedbins.
    
    Change-Id: I96ff01372da5c383cd3172999ea1dcf95a7862ce
    Signed-off-by: Joonwoo Park <joonwoop@codeaurora.org>
    Signed-off-by: therootlord <igor_cestari@hotmail.com>
    [kdrag0n: added missing sched_feat(ENERGY_AWARE) check]
    Signed-off-by: kdrag0n <dragon@khronodragon.com>
    Signed-off-by: Lau <laststandrighthere@gmail.com>

--- arch/arm64/kernel/topology.c
+++ arch/arm64/kernel/topology.c
@@ -309,6 +309,11 @@ static void update_cpu_capacity(unsigned int cpu)
 		cpu, arch_scale_cpu_capacity(NULL, cpu));
 }
 
+void update_cpu_power_capacity(int cpu)
+{
+	update_cpu_capacity(cpu);
+}
+
 static void update_siblings_masks(unsigned int cpuid)
 {
 	struct cpu_topology *cpu_topo, *cpuid_topo = &cpu_topology[cpuid];
@@ -370,7 +375,6 @@ void store_cpu_topology(unsigned int cpuid)
 
 topology_populated:
 	update_siblings_masks(cpuid);
-	update_cpu_capacity(cpuid);
 }
 
 static void __init reset_cpu_topology(void)
