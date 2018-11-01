0e6519044c08472f38595d09f11a34608cb583bf
Zhao Wei Liew <zhaoweiliew@gmail.com>
2018-11-01 10:05:30 +0000

    soc: qcom: haptic: Correct return value of show() methods
    By convention, these methods should return the number of bytes
    actually written into the buffer, not the number of bytes that
    would have been written.
    
    Change-Id: Iab8b292397bf3cbbf4a59a72812d3831ac7722c4
    Signed-off-by: Zhao Wei Liew <zhaoweiliew@gmail.com>

--- drivers/soc/qcom/qpnp-haptic.c
+++ drivers/soc/qcom/qpnp-haptic.c
@@ -1824,7 +1824,7 @@ static ssize_t qpnp_hap_min_show(struct device *dev,
 	struct qpnp_hap *hap = container_of(timed_dev, struct qpnp_hap,
 					 timed_dev);
 
-	return snprintf(buf, PAGE_SIZE, "%d\n", hap->vtg_min);
+	return scnprintf(buf, PAGE_SIZE, "%d\n", hap->vtg_min);
 }
 
 static ssize_t qpnp_hap_max_show(struct device *dev,
@@ -1834,7 +1834,7 @@ static ssize_t qpnp_hap_max_show(struct device *dev,
 	struct qpnp_hap *hap = container_of(timed_dev, struct qpnp_hap,
 					 timed_dev);
 
-	return snprintf(buf, PAGE_SIZE, "%d\n", hap->vtg_max);
+	return scnprintf(buf, PAGE_SIZE, "%d\n", hap->vtg_max);
 }
 
 static ssize_t qpnp_hap_default_show(struct device *dev,
@@ -1844,7 +1844,7 @@ static ssize_t qpnp_hap_default_show(struct device *dev,
 	struct qpnp_hap *hap = container_of(timed_dev, struct qpnp_hap,
 					 timed_dev);
 
-	return snprintf(buf, PAGE_SIZE, "%d\n", hap->vtg_default);
+	return scnprintf(buf, PAGE_SIZE, "%d\n", hap->vtg_default);
 }
 
 /* sysfs attributes */
