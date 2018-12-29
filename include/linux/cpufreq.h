b7e3fc592c40d26019add75fa3b712b24540c1b0
Augustine Lau <aug.lau22@gmail.com>
2018-12-28 21:33:28 -0800

    sched: Add HELIX_SCHEDUTIL

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -539,6 +539,9 @@ extern struct cpufreq_governor cpufreq_gov_schedutil;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BLU_SCHEDUTIL)
 extern struct cpufreq_governor cpufreq_gov_blu_schedutil;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_blu_schedutil)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_HELIX)
+extern struct cpufreq_governor cpufreq_gov_helix_schedutil;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_helix_schedutil)
 #endif
 
 static inline void cpufreq_policy_apply_limits(struct cpufreq_policy *policy)
