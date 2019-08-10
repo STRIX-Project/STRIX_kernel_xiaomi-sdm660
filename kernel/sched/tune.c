3d0ffe3c262cb3a001cb829c5b1570cc04effdc5
Julian Liu <wlootlxt123@gmail.com>
2019-08-10 21:23:32 +0800

    sched/tune: Set unlikely flag to !st_ta judgment
    * All Androids should have top-app stune
    * Optimize most of time since it often was called
    
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -1066,7 +1066,7 @@ int reset_stune_boost(int slot)
 	int ret = 0;
 	int boost = 0;
 
-	if (!st_ta)
+	if (unlikely(!st_ta))
 		return -EINVAL;
 
 	ret = deactivate_boost_slot(slot);
@@ -1087,7 +1087,7 @@ int reset_stune_boost(int slot)
 
 int do_stune_sched_boost(int *slot)
 {
-	if (!st_ta)
+	if (unlikely(!st_ta))
 		return -EINVAL;
 
 	return _do_stune_boost(st_ta->sched_boost, slot);
@@ -1095,7 +1095,7 @@ int do_stune_sched_boost(int *slot)
 
 int do_stune_boost(int boost, int *slot)
 {
-	if (!st_ta)
+	if (unlikely(!st_ta))
 		return -EINVAL;
 
 	return _do_stune_boost(boost, slot);
@@ -1103,7 +1103,7 @@ int do_stune_boost(int boost, int *slot)
 
 int get_sched_boost(void)
 {
-	if (!st_ta)
+	if (unlikely(!st_ta))
 		return -EINVAL;
 
 	return st_ta->sched_boost;
