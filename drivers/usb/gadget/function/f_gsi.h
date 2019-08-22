9e6a2975ada6c3e79d93393e01f4f7986fd120df
Elson Roy Serrao <eserrao@codeaurora.org>
2019-08-22 10:54:53 -0700

    usb: dwc3: Increase the TxFIFO resize factor
    With the TxFIFO depth calculated with the current resize
    factor, lot of flow control is observed with MTP file
    transfer. As a result the file transfer speed is not
    meeting the KPIs. Increasing this value so that
    flow control is minimized and throughput is improved.
    
    Change-Id: If64c2b6073df12edbcbb75fa08669f5268644e2a
    Signed-off-by: Elson Roy Serrao <eserrao@codeaurora.org>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- drivers/usb/gadget/function/f_gsi.h
+++ drivers/usb/gadget/function/f_gsi.h
@@ -414,7 +414,7 @@ static struct usb_ss_ep_comp_descriptor rmnet_gsi_ss_in_comp_desc = {
 	.bDescriptorType =	USB_DT_SS_ENDPOINT_COMP,
 
 	/* the following 2 values can be tweaked if necessary */
-	.bMaxBurst =		2,
+	.bMaxBurst =		6,
 	/* .bmAttributes =	0, */
 };
 
@@ -669,7 +669,7 @@ static struct usb_ss_ep_comp_descriptor rndis_gsi_ss_bulk_comp_desc = {
 	.bDescriptorType =	USB_DT_SS_ENDPOINT_COMP,
 
 	/* the following 2 values can be tweaked if necessary */
-	.bMaxBurst =		2,
+	.bMaxBurst =		6,
 	/* .bmAttributes =	0, */
 };
 
@@ -952,7 +952,7 @@ static struct usb_ss_ep_comp_descriptor mbim_gsi_ss_in_comp_desc = {
 	.bDescriptorType =      USB_DT_SS_ENDPOINT_COMP,
 
 	/* the following 2 values can be tweaked if necessary */
-	.bMaxBurst =         2,
+	.bMaxBurst =         6,
 	/* .bmAttributes =      0, */
 };
 
@@ -1250,7 +1250,7 @@ static struct usb_ss_ep_comp_descriptor ecm_gsi_ss_in_comp_desc = {
 	.bDescriptorType =	USB_DT_SS_ENDPOINT_COMP,
 
 	/* the following 2 values can be tweaked if necessary */
-	.bMaxBurst =         2,
+	.bMaxBurst =         6,
 	/* .bmAttributes =      0, */
 };
 
