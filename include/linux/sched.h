50c30848901fbb87729d02839b9836fa3f8c7cad
Ahmad Thoriq Najahi <najahiii@outlook.co.id>
2019-10-12 22:34:03 +0700

    ANDROID: staging: lmk: Checkout to k4.9
    Signed-off-by: Ahmad Thoriq Najahi <najahiii@outlook.co.id>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -2416,6 +2416,7 @@ static inline void memalloc_noio_restore(unsigned int flags)
 #define PFA_SPEC_SSB_FORCE_DISABLE	5	/* Speculative Store Bypass force disabled*/
 #define PFA_SPEC_IB_DISABLE		6	/* Indirect branch speculation restricted */
 #define PFA_SPEC_IB_FORCE_DISABLE	7	/* Indirect branch speculation permanently restricted */
+#define PFA_LMK_WAITING			7	/* Lowmemorykiller is waiting */
 
 
 #define TASK_PFA_TEST(name, func)					\
@@ -2452,6 +2453,9 @@ TASK_PFA_CLEAR(SPEC_IB_DISABLE, spec_ib_disable)
 
 TASK_PFA_TEST(SPEC_IB_FORCE_DISABLE, spec_ib_force_disable)
 TASK_PFA_SET(SPEC_IB_FORCE_DISABLE, spec_ib_force_disable)
+
+TASK_PFA_TEST(LMK_WAITING, lmk_waiting)
+TASK_PFA_SET(LMK_WAITING, lmk_waiting)
 
 /*
  * task->jobctl flags
