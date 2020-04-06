cf40fbe1f2706d6667d228fd50f9aaaa6c88e474
Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
2020-04-06 20:29:06 +0530

    drivers: touchscreen: nvt_touch_f7a: Disable debugging

--- drivers/input/touchscreen/nvt_touch_f7a/nt36xxx.h
+++ drivers/input/touchscreen/nvt_touch_f7a/nt36xxx.h
@@ -30,7 +30,7 @@
 
 #include "../lct_tp_info.h"
 
-#define NVT_DEBUG 1
+#define NVT_DEBUG 0
 
 
 #define NVTTOUCH_RST_PIN 66
@@ -49,11 +49,7 @@
 #define I2C_FW_Address 0x01
 #define I2C_HW_Address 0x62
 
-#if NVT_DEBUG
-#define NVT_LOG(fmt, args...)    pr_err("[%s] %s %d: " fmt, NVT_I2C_NAME, __func__, __LINE__, ##args)
-#else
 #define NVT_LOG(fmt, args...)    pr_info("[%s] %s %d: " fmt, NVT_I2C_NAME, __func__, __LINE__, ##args)
-#endif
 #define NVT_ERR(fmt, args...)    pr_err("[%s] %s %d: " fmt, NVT_I2C_NAME, __func__, __LINE__, ##args)
 
 #if 1
