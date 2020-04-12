cf845a32c359cbe9f9205649c3c5768f87424dc1
fiqri19102002 <fiqri15072019@gmail.com>
2020-04-12 19:31:33 +0700

    include: linux: Fix werror implicit declaration of function
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>

--- include/linux/syscalls.h
+++ include/linux/syscalls.h
@@ -216,10 +216,6 @@ static inline void addr_limit_user_check(void)
 		return;
 #endif
 
-	if (CHECK_DATA_CORRUPTION(!segment_eq(get_fs(), USER_DS),
-				  "Invalid address limit on user-mode return"))
-		force_sig(SIGKILL, current);
-
 #ifdef TIF_FSCHECK
 	clear_thread_flag(TIF_FSCHECK);
 #endif
