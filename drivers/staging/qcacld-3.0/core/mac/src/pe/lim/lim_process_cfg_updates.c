779b24a4c2b2aa1e8e8fdae8a5a59e18315cc2ab
Fiqri Ardyansyah <fiqri15072019@gmail.com>
2020-08-18 22:14:26 +0700

    drivers/staging: qcacld-3.0: Fix werror misleading indentation
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>

--- drivers/staging/qcacld-3.0/core/mac/src/pe/lim/lim_process_cfg_updates.c
+++ drivers/staging/qcacld-3.0/core/mac/src/pe/lim/lim_process_cfg_updates.c
@@ -155,9 +155,10 @@ void lim_handle_param_update(tpAniSirGlobal pMac, eUpdateIEsType cfgId)
 		msg.type = SIR_LIM_UPDATE_BEACON;
 		status = lim_post_msg_api(pMac, &msg);
 
-		if (status != TX_SUCCESS)
+		if (status != TX_SUCCESS) {
 			pe_err("Failed lim_post_msg_api %u", status);
 			break;
+		}
 	}
 	default:
 		break;
@@ -234,9 +235,10 @@ void lim_handle_cf_gparam_update(tpAniSirGlobal pMac, uint32_t cfgId)
 
 		status = lim_post_msg_api(pMac, &msg);
 
-		if (status != TX_SUCCESS)
+		if (status != TX_SUCCESS) {
 			pe_err("Failed lim_post_msg_api %u", status);
 			break;
+		}
 	}
 
 	case WNI_CFG_MPDU_DENSITY:
@@ -255,9 +257,10 @@ void lim_handle_cf_gparam_update(tpAniSirGlobal pMac, uint32_t cfgId)
 		pAmpduParamInfo->mpduDensity = (uint8_t) val2;
 		if (cfg_set_int
 			    (pMac, WNI_CFG_HT_AMPDU_PARAMS,
-			    *(uint8_t *) pAmpduParamInfo) != eSIR_SUCCESS)
+			    *(uint8_t *) pAmpduParamInfo) != eSIR_SUCCESS) {
 			pe_err("could not update HT AMPDU Param CFG");
 			break;
+		}
 	case WNI_CFG_MAX_RX_AMPDU_FACTOR:
 		if (wlan_cfg_get_int(pMac, WNI_CFG_HT_AMPDU_PARAMS, &val1) !=
 		    eSIR_SUCCESS) {
@@ -274,9 +277,10 @@ void lim_handle_cf_gparam_update(tpAniSirGlobal pMac, uint32_t cfgId)
 		pAmpduParamInfo->maxRxAMPDUFactor = (uint8_t) val2;
 		if (cfg_set_int
 			    (pMac, WNI_CFG_HT_AMPDU_PARAMS,
-			    *(uint8_t *) pAmpduParamInfo) != eSIR_SUCCESS)
+			    *(uint8_t *) pAmpduParamInfo) != eSIR_SUCCESS) {
 			pe_err("could not update HT AMPDU Param CFG");
 			break;
+		}
 
 	case WNI_CFG_DOT11_MODE:
 		if (wlan_cfg_get_int(pMac, WNI_CFG_DOT11_MODE, &val1) !=
