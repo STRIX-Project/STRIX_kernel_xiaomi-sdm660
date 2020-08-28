93e22a713ebb045d69aa6812f1caeabe88c2edd8
Fiqri Ardyansyah <fiqri15072019@gmail.com>
2020-08-28 14:18:24 +0700

    sched: electroutil: Fix Wmisleading indentation warning
    ../kernel/sched/cpufreq_electroutil.c: In function 'eugov_exit':
    ../kernel/sched/cpufreq_electroutil.c:928:2: warning: this 'if' clause does not guard... [-Wmisleading-indentation]
      928 |  if (!count)
          |  ^~
    ../kernel/sched/cpufreq_electroutil.c:930:3: note: ...this statement, but the latter is misleadingly indented as if it were guarded by the 'if'
      930 |   eugov_tunables_free(tunables);
          |   ^~~~~~~~~~~~~~~~~~~
    ../kernel/sched/cpufreq_schedutil.c:357:3: warning: this 'if' clause does not guard... [-Wmisleading-indentation]
      357 |   if (busy && next_f < sg_policy->next_freq &&
          |   ^~
    ../kernel/sched/cpufreq_schedutil.c:361:4: note: ...this statement, but the latter is misleadingly indented as if it were guarded by the 'if'
      361 |    sg_policy->cached_raw_freq = 0;
          |    ^~~~~~~~~
    
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>

--- kernel/sched/cpufreq_electroutil.c
+++ kernel/sched/cpufreq_electroutil.c
@@ -925,9 +925,10 @@ static int eugov_exit(struct cpufreq_policy *policy)
 
 	count = gov_attr_set_put(&tunables->attr_set, &eg_policy->tunables_hook);
 	policy->governor_data = NULL;
-	if (!count)
+	if (!count) {
 		eugov_tunables_save(policy, tunables);
 		eugov_tunables_free(tunables);
+	}
 
 	mutex_unlock(&global_tunables_lock);
 
