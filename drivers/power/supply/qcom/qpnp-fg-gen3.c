8f6cfe761a3da9f4c6fc36c7dc701907e93c031d
Fiqri Ardyansyah <fiqri15072019@gmail.com>
2020-09-11 20:07:45 +0700

    drivers/power: qcom: Silence debug messages
    * Silence useless debug messages.
    * It only raises spam message on dmsg while charging.
    
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>

--- drivers/power/supply/qcom/qpnp-fg-gen3.c
+++ drivers/power/supply/qcom/qpnp-fg-gen3.c
@@ -724,7 +724,7 @@ static int fg_get_battery_temp(struct fg_chip *chip, int *val)
 		if (quiet_them)
 			rc = thermal_zone_get_temp(quiet_them, &temp);
 		temp = (temp - 3) * 10;
-		pr_err("LCT USE QUIET_THERM AS BATTERY TEMP \n");
+		pr_debug("LCT USE QUIET_THERM AS BATTERY TEMP \n");
 	}
 #endif
 #endif
@@ -4096,7 +4096,7 @@ static int fg_bcl_reset(struct fg_chip *chip)
 		pr_err("Error in writing peek mux1, rc=%d\n", rc);
 		return rc;
 	}
-	pr_err("FG_BCL_RESET PEEK_MUX = %d\n",peek_mux);
+	pr_debug("FG_BCL_RESET PEEK_MUX = %d\n",peek_mux);
 	val = 0x83;
 	rc = fg_write(chip, BATT_INFO_PEEK_MUX1(chip), &val, 1);
 	if (rc < 0) {
@@ -4106,7 +4106,7 @@ static int fg_bcl_reset(struct fg_chip *chip)
 
 	mutex_lock(&chip->sram_rw_lock);
 	for (i = 0; i < BCL_RESET_RETRY_COUNT; i++) {
-		pr_err("FG_BCL_RESET RETRY\n");
+		pr_debug("FG_BCL_RESET RETRY\n");
 		rc = fg_dma_mem_req(chip, true);
 		if (rc < 0) {
 			pr_err("Error in locking memory, rc=%d\n", rc);
@@ -4118,9 +4118,9 @@ static int fg_bcl_reset(struct fg_chip *chip)
 			pr_err("Error in reading rdback, rc=%d\n", rc);
 			goto release_mem;
 		}
-		pr_err("FG_BCL_RESET VAL = %d\n",val);
+		pr_debug("FG_BCL_RESET VAL = %d\n",val);
 		if (val & PEEK_MUX1_BIT) {
-			pr_err("FG_BCL_RESET DEBUG\n");
+			pr_debug("FG_BCL_RESET DEBUG\n");
 			rc = fg_masked_write(chip, BATT_SOC_RST_CTRL0(chip),
 						BCL_RESET_BIT, BCL_RESET_BIT);
 			if (rc < 0) {
@@ -4151,7 +4151,7 @@ static int fg_bcl_reset(struct fg_chip *chip)
 				return rc;
 			}
 			success = false;
-			pr_err_ratelimited("PEEK_MUX1 not set retrying...\n");
+			pr_debug("PEEK_MUX1 not set retrying...\n");
 			msleep(1000);
 		}
 	}
