167029ac6b5e37c1ce7f012bccebee77981c52ff
Fiqri Ardyansyah <fiqri15072019@gmail.com>
2021-03-04 16:52:53 +0700

    msm: kgsl: Squash revert some commits
    This revert following commits:
    
    90a9d91: msm: kgsl: Report correct GPU frequency for clock_mhz
    8e588c3: msm: kgsl: Report correct GPU frequency in sysfs
    40ba851: msm: kgsl: Remove excessive log in kgsl_snapshot_get_object
    416faef: msm: kgsl: Ensure GPU gdscs are off in SLUMBER and during hard reset
    746163c: msm: kgsl: Remove extra call to turn off interrupts
    0e5783d: msm: kgsl: Fix drawqueue timer race condition
    375ba4f: msm: kgsl: improve memory stats maintenance
    
    Some of these changes only cause poor rendering speed, so revert
    some of these changes to bring the render speed back to normal.
    
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -4721,8 +4721,6 @@ struct kgsl_driver kgsl_driver  = {
 	.stats.secure_max = ATOMIC_LONG_INIT(0),
 	.stats.mapped = ATOMIC_LONG_INIT(0),
 	.stats.mapped_max = ATOMIC_LONG_INIT(0),
-	.stats.page_free_pending = ATOMIC_LONG_INIT(0),
-	.stats.page_alloc_pending = ATOMIC_LONG_INIT(0),
 };
 EXPORT_SYMBOL(kgsl_driver);
 
