40613fd985db44fcfcf74b5b4e6ccdc774eb4c9c
Fiqri Ardyansyah <fiqri15072019@gmail.com>
2021-03-25 10:14:24 +0700

    soc: qcom: Remove changes import from jason device
    The lavender, tulip, wayne, and whyred devices don't need a
    change from jason aka MI Note 3, so remove those changes.
    
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>

--- kernel/power/suspend.c
+++ kernel/power/suspend.c
@@ -4,7 +4,6 @@
  * Copyright (c) 2003 Patrick Mochel
  * Copyright (c) 2003 Open Source Development Lab
  * Copyright (c) 2009 Rafael J. Wysocki <rjw@sisk.pl>, Novell Inc.
- * Copyright (C) 2019 XiaoMi, Inc.
  *
  * This file is released under the GPLv2.
  */
@@ -32,14 +31,11 @@
 #include <linux/moduleparam.h>
 #include <linux/wakeup_reason.h>
 
-#include <linux/gpio.h>
 #include "power.h"
 
 const char *pm_labels[] = { "mem", "standby", "freeze", NULL };
 const char *pm_states[PM_SUSPEND_MAX];
 
-extern int PROC_AWAKE_ID; /* 12th bit */
-extern int slst_gpio_base_id;
 unsigned int pm_suspend_global_flags;
 EXPORT_SYMBOL_GPL(pm_suspend_global_flags);
 
@@ -553,11 +549,7 @@ int pm_suspend(suspend_state_t state)
 	if (state <= PM_SUSPEND_ON || state >= PM_SUSPEND_MAX)
 		return -EINVAL;
 
-	gpio_set_value (slst_gpio_base_id+PROC_AWAKE_ID, 0);
-	pr_debug("%s: PM_SUSPEND_PREPARE %d \n", __func__, slst_gpio_base_id + PROC_AWAKE_ID);
 	error = enter_state(state);
-	gpio_set_value (slst_gpio_base_id+PROC_AWAKE_ID, 1);
-	pr_debug("%s: PM_POST_SUSPEND %d \n", __func__, slst_gpio_base_id + PROC_AWAKE_ID);
 	if (error) {
 		suspend_stats.fail++;
 		dpm_save_failed_errno(error);
