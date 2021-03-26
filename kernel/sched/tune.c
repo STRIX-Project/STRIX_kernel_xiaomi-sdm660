7b2953ba92c8122fde1d6c7cd891e2b9f0a763bb
Fiqri Ardyansyah <fiqri15072019@gmail.com>
2021-03-26 22:29:53 +0700

    sched: tune: Don't prefer foreground to sched_boost
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -898,7 +898,7 @@ static void write_default_values(struct cgroup_subsys_state *css)
 	static struct st_data st_targets[] = {
 		{ "audio-app",	0, 0, 0 },
 		{ "background",	0, 0, 0 },
-		{ "foreground",	0, 0, 5 },
+		{ "foreground",	0, 0, 0 },
 		{ "rt",		0, 0, 0 },
 		{ "top-app",	0, 1, 0 },
 	};
