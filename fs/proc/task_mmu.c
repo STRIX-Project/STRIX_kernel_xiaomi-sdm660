e7014ab412876ac3e14a072c8fd28e146278346e
Vinayak Menon <vinmenon@codeaurora.org>
2021-04-07 14:39:13 +0700

    BACKPORT: mm: process_reclaim: consider compound pages
    Avoid passing tail pages to isolate_lru_page. In the case
    process reclaim, since only lru pages are considered, this
    is just to avoid a warning from isolate_lru_page.
    
    Change-Id: I1f54dcec15f8c2d5ba16738657e79d2793d36c77
    Signed-off-by: Vinayak Menon <vinmenon@codeaurora.org>
    (cherry-picked from commit 2a580c4466231c19452284c9a4cca22486ae8967)
    Signed-off-by: Albert I <kras@raphielgang.org>
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -1548,7 +1548,7 @@ cont:
 		if (page_mapcount(page) != 1)
 			continue;
 
-		if (isolate_lru_page(page))
+		if (isolate_lru_page(compound_head(page)))
 			continue;
 
 		list_add(&page->lru, &page_list);
