b7031e673fab15a8221fc310368fcd15dc526a85
Fiqri Ardyansyah <fiqri15072019@gmail.com>
2021-04-14 09:30:35 +0700

    Revert "msm: camera_v2: Fix structs for jasmine 10 blobs"
    This reverts commit 862ea3e4ec325240b1d77013f8d6043c0e8c3e12.
    
    This commit is no longer needed because all member structs
    are already defined in CONFIG_MACH_XIAOMI_SDM660 and this change
    only makes the compile an error when enabled new camera blob
    on wayne device.
    
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>

--- include/uapi/media/msm_camsensor_sdk.h
+++ include/uapi/media/msm_camsensor_sdk.h
@@ -326,7 +326,7 @@ struct msm_vcm_id_info_t {
 	enum msm_cci_i2c_master_t cci_i2c_master;
 };
 
-#if defined(CONFIG_MACH_XIAOMI_NEW_CAMERA) || defined(CONFIG_MACH_XIAOMI_WAYNE)
+#if defined(CONFIG_MACH_XIAOMI_NEW_CAMERA)
 struct msm_lens_id_info_t {
 	unsigned short eeprom_slave_addr;
 	unsigned short lens_id_addr;
@@ -350,12 +350,10 @@ struct msm_camera_sensor_slave_info {
 	enum msm_camera_i2c_reg_addr_type addr_type;
 	struct msm_sensor_id_info_t sensor_id_info;
 #ifdef CONFIG_MACH_XIAOMI_SDM660
-#ifndef CONFIG_MACH_XIAOMI_WAYNE
 	struct msm_vendor_id_info_t vendor_id_info;
 	struct msm_vcm_id_info_t vcm_id_info;
 #ifdef CONFIG_MACH_XIAOMI_NEW_CAMERA
 	struct msm_lens_id_info_t lens_id_info;
-#endif
 #endif
 #endif
 	struct msm_sensor_power_setting_array power_setting_array;
@@ -363,11 +361,6 @@ struct msm_camera_sensor_slave_info {
 	struct msm_sensor_init_params sensor_init_params;
 	enum msm_sensor_output_format_t output_format;
 	uint8_t bypass_video_node_creation;
-#ifdef CONFIG_MACH_XIAOMI_WAYNE
-	struct msm_vendor_id_info_t vendor_id_info;
-	struct msm_vcm_id_info_t vcm_id_info;
-	struct msm_lens_id_info_t lens_id_info;
-#endif
 };
 
 struct msm_camera_i2c_reg_array {
